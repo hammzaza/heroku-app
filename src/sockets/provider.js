e79fb3350bad76c442902def2c07c6423bd86df8
Hamza Ali <hamza.ali5542@gmail.com>
2018-10-27 17:33:34 +0500

    Added changes

--- src/sockets/provider.js
+++ src/sockets/provider.js
@@ -1,9 +1,13 @@
 module.exports = function(server){
     var io = require('socket.io').listen(server);
     io.on('connection', function(socket){
+        console.log('connected');
         socket.on('SensorData', function(data){
             //you have temp,humidity,co2level,lightintensity in a json format.
             //save this in database
+        });
+        socket.on('StopController',function(response){
+            console.log(response);
         });
     });
 };
