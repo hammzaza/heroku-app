1727b6711f865cb1b7a6ac64889961254db5c816
Hamza Ali <hamza.ali5542@gmail.com>
2018-10-27 20:21:42 +0500

    testing

--- src/sockets/provider.js
+++ src/sockets/provider.js
@@ -2,9 +2,10 @@ module.exports = function(server){
     var io = require('socket.io').listen(server);
     io.on('connection', function(socket){
         console.log('connected');
-        socket.on('SensorData', function(data){
+        socket.on('sensorData', function(data){
             //you have temp,humidity,co2level,lightintensity in a json format.
             //save this in database
+            console.log(data);
         });
     });
 };
