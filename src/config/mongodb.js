e7f8c56cd76235e5141e688fd37c5072a22d9072
Hamza Ali <hamza.ali5542@gmail.com>
2018-10-27 19:18:43 +0500

    mongo atlas working

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,3 +1,3 @@
-var url = 'mongodb://localhost:27017/fyp';
+var url = 'mongodb://hamza:mysecretpassword@fyp-web-shard-00-00-nielc.mongodb.net:27017,fyp-web-shard-00-01-nielc.mongodb.net:27017,fyp-web-shard-00-02-nielc.mongodb.net:27017/my-db?ssl=true&replicaSet=fyp-web-shard-0&authSource=admin&retryWrites=true';
 
 module.exports = url;
