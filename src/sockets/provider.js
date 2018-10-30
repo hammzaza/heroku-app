6dec77c5d7710f90c60c93527f55adac69c5d69b
Hamza Ali <hamza.ali5542@gmail.com>
2018-10-30 19:30:50 +0500

    changed json

--- src/sockets/provider.js
+++ src/sockets/provider.js
@@ -4,17 +4,17 @@ module.exports = function(server){
     io.on('connection', function(socket){
         console.log('connected');
         socket.on('sensorData', function(data){
-            console.log(data);
+            sen= JSON.parse(data);
             sensorData = new Sensor();
-            sensorData.temperature = data.temperature;
-            sensorData.humidity = data.humidity;
-            sensorData.visible = data.visible;
-            sensorData.infrared = data.infrared;
+            sensorData.temperature = sen.temperature;
+            sensorData.humidity = sen.humidity;
+            sensorData.visible = sen.visible;
+            sensorData.infrared = sen.infrared;
             sensorData.save(function(err) {
                 if (err)
                     throw err;
             });
-            io.emit('sensor',JSON.parse(data));
+            io.emit('sensor',sen);
         });
         socket.on('updateState',function(data){
             console.log(data)
