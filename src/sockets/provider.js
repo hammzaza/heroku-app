73288a01f4689d9bccf960b19cd30780794360fd
Hamza Ali <hamza.ali5542@gmail.com>
2018-11-14 01:15:04 +0500

    added code

--- src/sockets/provider.js
+++ src/sockets/provider.js
@@ -5,36 +5,32 @@ module.exports = function(server){
         check = false;
         console.log('connected');
         socket.on('Start',function(condition){
-            check = true;
-            console.log('you started me')
             io.emit('StartSensor',true);
             io.emit('StartScript',true);
-            socket.on('sensorData', function(data){
-                io.emit('sensor',data);
-                console.log(data);
-                
-            });
-            socket.on('gpsd',function(loc){
-                console.log(data);
-                console.log(loc.lat);
-                console.log(loc.lon)
-                io.emit('gpsData',data);
-            });
-            socket.on('updateState',function(data){
-                console.log(data);
-                io.emit('MoveController',data);
-            });
-            socket.on('Stop',function(data){
-                io.emit('StopController',false);
-            });
-            socket.on('stopsocket',function(state){
-                check = false;
-                console.log('you ended me');
-                io.emit('exitsocket',false);
-                io.emit('exitsensor',false);
-            });
         });
-        
-        
-    });
+        socket.on('sensorData', function(data){
+            io.emit('sensor',data);
+            console.log(data);
+            
+        });
+        socket.on('gpsd',function(loc){
+            console.log(data);
+            console.log(loc.lat);
+            console.log(loc.lon)
+            io.emit('gpsData',data);
+        });
+        socket.on('updateState',function(data){
+            console.log(data);
+            io.emit('MoveController',data);
+        });
+        socket.on('Stop',function(data){
+            io.emit('StopController',false);
+        });
+        socket.on('stopsocket',function(state){
+            check = false;
+            console.log('you ended me');
+            io.emit('exitsocket',false);
+            io.emit('exitsensor',false);
+        });
+        });
 }
