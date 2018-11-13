db0a492c2a0526b038f4a0e58ad9d38ce8111dfd
Hamza Ali <hamza.ali5542@gmail.com>
2018-11-13 22:40:46 +0500

    fixed errors

--- src/sockets/provider.js
+++ src/sockets/provider.js
@@ -2,35 +2,35 @@ var Sensor = require('../schemas/sensors');
 module.exports = function(server){
     var io = require('socket.io').listen(server);
     io.on('connection', function(socket){
+        check = false;
         console.log('connected');
         socket.on('Start',function(condition){
             io.emit('StartSensor',true);
             io.emit('StartScript',true);
-
-            socket.on('sensorData', function(data){
-                io.emit('sensor',data);
+        });
+        socket.on('sensorData', function(data){
+            io.emit('sensor',data);
+            console.log(data);
+            socket.on('Gps_Data',function(loc){
                 console.log(data);
-                socket.on('Gps_Data',function(loc){
-                    console.log(data);
-                    console.log(loc.lat);
-                    console.log(loc.lon)
-                    io.emit('gpsData',data);
-    
-                    sens = new Sensor();
-                    sens.ppm =  parseFloat(data)
-                    sens.lat =  parseFloat(loc.lat);
-                    sens.lon =  parseFloat(loc.lon)
-                    sen.save(function(err) {
-                        if (err)
-                            throw err;
-                    });
+                console.log(loc.lat);
+                console.log(loc.lon)
+                io.emit('gpsData',data);
+
+                sens = new Sensor();
+                sens.ppm =  parseFloat(data)
+                sens.lat =  parseFloat(loc.lat);
+                sens.lon =  parseFloat(loc.lon)
+                sen.save(function(err) {
+                    if (err)
+                        throw err;
                 });
             });
-            socket.on('updateState',function(data){
-                console.log(data);
-                
-                io.emit('MoveController',data);
-            });
+        });
+        socket.on('updateState',function(data){
+            console.log(data);
+            
+            io.emit('MoveController',data);
         });
         socket.on('Stop',function(state){
             io.emit('exitsocket',false);
